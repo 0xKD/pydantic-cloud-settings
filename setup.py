a1120c6c3b3b8fca2db6b169b867750729d04d08
Kedar <kedardeore@gmail.com>
2021-11-10 14:38:20 +0530

    Make actual fix for previous release :D

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ import setuptools
 setuptools.setup(install_requires=[
     "pydantic>=1.7.0,<1.8",
     "python-dotenv>=0.17,<0.18",
-    "google-cloud-secret-manager~=2.2.0",
+    "google-cloud-secret-manager>=2.2.0,<3",
 ])
